02d27e264195d0b7122593c862a4f33bb7a63871
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-09-05 14:09:26 +0600

    login_screen

--- lib/ui/screen/login_screen.dart
+++ lib/ui/screen/login_screen.dart
@@ -105,7 +105,7 @@ class _LoginScreenState extends State<LoginScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Don't have an account"),
+                  Text("Don't have  account"),
                   TextButton(
                     onPressed: () {
                       Navigator.push(

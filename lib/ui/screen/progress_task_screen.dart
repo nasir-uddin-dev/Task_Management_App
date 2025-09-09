23f9b3a502870e24cafbb43afc40d5289ad10af7
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-09-09 14:22:44 +0600

    progres_task

--- lib/ui/screen/progress_task_screen.dart
+++ lib/ui/screen/progress_task_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/task_count_by_status_card.dart';
+
 class ProgressTaskScreen extends StatefulWidget {
   const ProgressTaskScreen({super.key});
 
@@ -11,22 +13,28 @@ class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Card(
-            elevation: 0,
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Column(
-                children: [
-                  Text("09"),
-                  Text("New")
-                ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 16,),
+            SizedBox(
+              height: 90,
+              child: ListView.separated(
+                itemCount: 4,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return TaskCountByStatusCard(title: 'New', count: 2);
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return SizedBox(
+                    width: 4,
+                  );
+                },
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},

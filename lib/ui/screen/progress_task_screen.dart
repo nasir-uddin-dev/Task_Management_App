e1b27f99270647c49e103b2b3e6239983ebe9c12
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-09-09 13:46:37 +0600

    progres_task

--- lib/ui/screen/progress_task_screen.dart
+++ lib/ui/screen/progress_task_screen.dart
@@ -11,6 +11,23 @@ class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Column(
+        children: [
+          Card(
+            elevation: 0,
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  Text("09"),
+                  Text("New")
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.add),
